1f6a80a46b37c25784d8c5adb94b4cce6e0d9b62
Felix <felixr91@gmail.com>
2018-11-09 15:53:44 -0700

    itunes music search

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -7,9 +7,25 @@ const itunesService = new ItunesService()
 function drawSongs(results) {
   console.log(results)
   //YOUR CODING STARTS HERE
-
-
-
+  let template = ''
+  for (var i = 0; i < results.length; i++) {
+    template += `
+      <div class="col-4 pb-5">
+        <div class="card d-flex justify-content-center">
+          <img class="card-img-top" src="${results[i].albumArt}" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">${results[i].title}</p>
+              <p class="card-text">${results[i].artist}</p>
+              <p class="card-text">${results[i].collection}</p>
+              <p class="card-text">${results[i].price}</p>
+              <audio controls>
+                  <source src="${results[i].preview}" type="audio/mpeg">
+              </audio>
+            </div>
+        </div>
+      </div>`
+  }
+  document.getElementById("songs").innerHTML = template;
 }
 
 
